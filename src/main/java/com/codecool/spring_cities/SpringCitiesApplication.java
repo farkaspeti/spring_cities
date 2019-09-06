e2d90d9d6e7d9f0f8f69d44997f02514e6c7acad
farkaspeti <farkas.peti97@gmail.com>
2019-09-06 13:26:21 +0200

    base of the Spring project generated via IntelliJ

---
+++ src/main/java/com/codecool/spring_cities/SpringCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.spring_cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCitiesApplication.class, args);
+    }
+
+}
