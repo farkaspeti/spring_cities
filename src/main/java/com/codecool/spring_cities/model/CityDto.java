7df041cf5b2533a9ca2c3255821d440adb0bf806
farkaspeti <farkas.peti97@gmail.com>
2019-09-09 11:34:12 +0200

    cityDto implemented

--- src/main/java/com/codecool/spring_cities/model/CityDto.java
+++ src/main/java/com/codecool/spring_cities/model/CityDto.java
@@ -1,7 +1,18 @@
 package com.codecool.spring_cities.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CityDto implements Serializable {
+    
+    private Long id;
+    private String cityName;
+    private Long population;
 
 }
