a6a85d123c0614bc12d993db85a1a6dc2aafa54d
farkaspeti <farkas.peti97@gmail.com>
2019-09-10 09:41:58 +0200

    HouseRepository now native query free

--- src/main/java/com/codecool/spring_cities/repositories/HouseRepository.java
+++ src/main/java/com/codecool/spring_cities/repositories/HouseRepository.java
@@ -1,13 +1,14 @@
 package com.codecool.spring_cities.repositories;
 
 import com.codecool.spring_cities.entities.HouseEntity;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HouseRepository extends CrudRepository<HouseEntity, Long> {
-    @Query(value = "SELECT * FROM houses WHERE city_id = :id", nativeQuery = true)
-    HouseEntity getHouseByCityId(@Param("id") Long cityId);
+    
+    Optional<HouseEntity> findById(@Param("id") Long cityId);
 }
