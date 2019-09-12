373bded7e572d77c1c3a5197c99e0d90141c56d7
farkaspeti <farkas.peti97@gmail.com>
2019-09-12 20:48:40 +0200

    StatDto created

---
+++ src/main/java/com/codecool/spring_cities/model/StatDto.java
@@ -0,0 +1,16 @@
+package com.codecool.spring_cities.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatDto implements Serializable {
+    
+    private double result;
+}
