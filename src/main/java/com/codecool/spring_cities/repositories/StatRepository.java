09078cbaffe756b695e2e37f13206c586d616137
farkaspeti <farkas.peti97@gmail.com>
2019-09-18 15:12:59 +0200

    averagePopulation Query implemented

--- src/main/java/com/codecool/spring_cities/repositories/StatRepository.java
+++ src/main/java/com/codecool/spring_cities/repositories/StatRepository.java
@@ -14,5 +14,6 @@ public interface StatRepository extends CrudRepository<StatEntity, Long>{
     @Query(value = "SELECT AVG(HOUSE_RESIDENTS) FROM HOUSES WHERE CITY_ENTITY_ID =?",nativeQuery = true)
     double averageResidentsInCity(Long cityId);
     
-      //double averagePopulationInCity(Long cityId);
+    @Query(value = "SELECT SUM(HOUSE_RESIDENTS) FROM HOUSES",nativeQuery = true)
+    double averagePopulationInCity(Long cityId);
 }
