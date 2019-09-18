b9f14bb4a89b88ff711a4408f60f87218f2eb5e8
farkaspeti <farkas.peti97@gmail.com>
2019-09-18 15:26:53 +0200

    exception handler for AopInvocationException implemented

--- src/main/java/com/codecool/spring_cities/handlers/ServiceExceptionHandler.java
+++ src/main/java/com/codecool/spring_cities/handlers/ServiceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.codecool.spring_cities.handlers;
 
 import com.codecool.spring_cities.exceptions.ServiceException;
 import com.codecool.spring_cities.model.ErrorDto;
+import org.springframework.aop.AopInvocationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +18,11 @@ public class ServiceExceptionHandler {
     
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ErrorDto> serviceExceptionHandler(ServiceException ex) {
+        return new ResponseEntity<>(new ErrorDto(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+    @ExceptionHandler(AopInvocationException.class)
+    public ResponseEntity<ErrorDto> AopInvocationExceptionHandler(AopInvocationException ex) {
         return new ResponseEntity<>(new ErrorDto(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
