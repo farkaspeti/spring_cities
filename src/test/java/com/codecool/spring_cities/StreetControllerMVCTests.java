63a5b74b48aaa4a8869a1ec671b91fb8c200a871
farkaspeti <farkas.peti97@gmail.com>
2019-10-11 14:01:18 +0200

    test push for jenkins6.

--- src/test/java/com/codecool/spring_cities/StreetControllerMVCTests.java
+++ src/test/java/com/codecool/spring_cities/StreetControllerMVCTests.java
@@ -1,13 +1,12 @@
 package com.codecool.spring_cities;
 
 
-import com.codecool.spring_cities.controllers.CityController;
 import com.codecool.spring_cities.controllers.StreetController;
 import com.codecool.spring_cities.entities.StreetEntity;
 import com.codecool.spring_cities.services.StreetService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
