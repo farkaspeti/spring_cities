e38482073b4661ea614b8579e9a1f2b77ca9166c
farkaspeti <farkas.peti97@gmail.com>
2019-10-11 10:18:53 +0200

    test push for jenkins2

--- src/test/java/com/codecool/spring_cities/CityControllerMVCTests.java
+++ src/test/java/com/codecool/spring_cities/CityControllerMVCTests.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
